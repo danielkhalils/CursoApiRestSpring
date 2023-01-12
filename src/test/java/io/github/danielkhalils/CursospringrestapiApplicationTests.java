e8a2c2f3337498cb86c0a209f267bdbfdd44b754
danielkhalils <dkses.hef@uea.edu.br>
2023-01-12 18:14:18 -0400

    Inicializando projeto API Rest Spring Boot

---
+++ src/test/java/io/github/danielkhalils/CursospringrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielkhalils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
