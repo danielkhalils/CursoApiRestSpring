113471aa575f7b236c3e4c76e65a953096507df0
danielkhalils <dkses.hef@uea.edu.br>
2023-01-12 22:28:43 -0400

    Passando parâmetros para o serviço Restful - Lista

--- src/main/java/io/github/danielkhalils/controller/IndexController.java
+++ src/main/java/io/github/danielkhalils/controller/IndexController.java
@@ -1,11 +1,16 @@
 package io.github.danielkhalils.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.danielkhalils.model.Usuario;
 
 @RestController
 @RequestMapping(value = "/usuario")
@@ -23,5 +28,32 @@ public class IndexController {
 		System.out.print("idade sendo recebida " + idade);
 		return new ResponseEntity("Sua idade é " + idade, HttpStatus.OK);
 	}
+	
+	/*Passando uma lista de usuários*/
+	@GetMapping(value = "/lista", produces = "apllication/json")
+	public ResponseEntity<Usuario> init() {
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setLogin("daniel.khalil.dkss@gmail.com");
+		usuario.setNome("Daniel Khalil");
+		usuario.setSenha("123");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(2L);
+		usuario2.setLogin("Adaniel.khalil.dkss@gmail.com");
+		usuario2.setNome("ADaniel Khalil");
+		usuario2.setSenha("1123");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
+	}
+	
+	
+	
+	
+	
 	
 }
